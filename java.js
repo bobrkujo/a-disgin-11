ebdb9d2fb28329a29d7c2c8f3d6247c131f47a59
bobrkujo <uwkrin@gmail.com>
2025-12-04 15:47:50 +0500

    Delete java.js

--- java.js
+++
@@ -1,8 +0,0 @@
-let first = confirm("хочешь попробовать мой калькулятор?");
-if (first == true) {
-  let second = prompt("введи первое число")
-    let third = prompt("введи второе число")
-    alert("результат: " + (Number(second) + Number(third)))    
-} else {
-  alert("Хай гитлер")
-}
